5230ad159b2ee4b6898ae8d155d3f7c818f9bd07
Eggcode39 <cesar.ruiz39124@gmail.com>
2021-05-21 10:34:40 -0500

    Correción Citas

--- app/controllers/CitaController.php
+++ app/controllers/CitaController.php
@@ -6,36 +6,98 @@
  * Time: 19:00
  */
 require 'app/models/Cita.php';
+require 'app/models/Paciente.php';
 class CitaController{
     private $log;
     private $cita;
+    private $paciente;
 
     public function __construct()
     {
         $this->log = new Log();
         $this->cita = new Cita();
+        $this->paciente = new Paciente();
     }
 
     public function registrar_cita(){
         try{
+            $cita_registrada = [];
             $model = new Cita();
             //If All OK, the message does not change
             $message = "Code 1: Ok, Code 2: Error";
+            $mt = microtime(true);
             if(isset($_POST['id_paciente']) && isset($_POST['cita_fecha']) && isset($_POST['cita_hora'])){
                 $model->id_paciente = $_POST['id_paciente'];
                 $model->cita_fecha = $_POST['cita_fecha'];
                 $model->cita_hora = $_POST['cita_hora'];
+                $model->cita_mt = $mt;
                 $result = $this->cita->registrar_cita($model);
             } else {
                 $result = 6;
                 $message = "Code 6: Datos No Recibidos";
+            }
+
+            if($result == 1){
+                $cita_registrada = $this->cita->buscar_cita_paciente_mt($mt);
             }
         } catch (Exception $e){
             $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
             $result = 2;
             $message = "Code 2: General Error";
         }
-        $response = array("code" => $result,"message" => $message);
+        $response = array("code" => $result,"message" => $message, "cita_registrada" => $cita_registrada);
+        $data = array("result" => $response);
+        echo json_encode($data);
+    }
+
+    public function registrar_cita_dni(){
+        try{
+            $cita_registrada = [];
+            $model = new Cita();
+            //If All OK, the message does not change
+            $message = "Code 1: Ok, Code 2: Error";
+            $mt = microtime(true);
+            if(isset($_POST['dni']) && isset($_POST['cita_fecha']) && isset($_POST['cita_hora'])){
+                $paciente = $this->cita->buscar_paciente_dni($_POST['dni']);
+                if(isset($paciente->id_paciente)){
+                    $model->id_paciente = $paciente->id_paciente;
+                    $model->cita_fecha = $_POST['cita_fecha'];
+                    $model->cita_hora = $_POST['cita_hora'];
+                    $model->cita_mt = $mt;
+                    $result = $this->cita->registrar_cita($model);
+                } else {
+                    $model_p = new Paciente();
+                    $model_p->paciente_nombre = 'Consulta';
+                    $model_p->paciente_apellido = 'Ambulatoria';
+                    $model_p->paciente_dni = $_POST['dni'];
+                    $model_p->paciente_telefono = '999999999';
+                    $model_p->paciente_correo = 'no-correo@gmail.com';
+                    $model_p->paciente_imagen_url = "https://guabba.com/dental/media/user.jpg";
+                    $result_p = $this->paciente->registrar_paciente($model_p);
+                    if($result_p == 1){
+                        $paciente = $this->cita->buscar_paciente_dni($_POST['dni']);
+
+                        $model->id_paciente = $paciente->id_paciente;
+                        $model->cita_fecha = $_POST['cita_fecha'];
+                        $model->cita_hora = $_POST['cita_hora'];
+                        $model->cita_mt = $mt;
+                        $result = $this->cita->registrar_cita($model);
+                    }
+                }
+            } else {
+                $result = 6;
+                $message = "Code 6: Datos No Recibidos";
+            }
+
+            if($result == 1){
+                $cita_registrada = $this->cita->buscar_cita_paciente_mt($mt);
+            }
+        } catch (Exception $e){
+            $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
+            $result = 2;
+            $message = "Code 2: General Error";
+        }
+        $response = array("code" => $result,"message" => $message, "cita_registrada" => $cita_registrada);
         $data = array("result" => $response);
         echo json_encode($data);
     }
--- app/models/Cita.php
+++ app/models/Cita.php
@@ -15,14 +15,15 @@ class Cita{
 
     public function registrar_cita($model){
         try{
-            $sql = "insert into cita (id_paciente, cita_fecha, cita_hora, cita_comentarios, cita_estado) values (?,?,?,?,?)";
+            $sql = "insert into cita (id_paciente, cita_fecha, cita_hora, cita_comentarios, cita_estado, cita_mt) values (?,?,?,?,?,?)";
             $stm = $this->pdo->prepare($sql);
             $stm->execute([
                 $model->id_paciente,
                 $model->cita_fecha,
                 $model->cita_hora,
                 "",
-                0
+                0,
+                $model->cita_mt
             ]);
             $result = 1;
         } catch (Exception $e){
@@ -54,6 +55,32 @@ class Cita{
             $stm = $this->pdo->prepare($sql);
             $stm->execute([$fecha]);
             $result = $stm->fetchAll();
+        } catch (Exception $e){
+            $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
+            $result = [];
+        }
+        return $result;
+    }
+
+    public function buscar_paciente_dni($dni){
+        try{
+            $sql = "select * from paciente where paciente_dni = ?";
+            $stm = $this->pdo->prepare($sql);
+            $stm->execute([$dni]);
+            $result = $stm->fetch();
+        } catch (Exception $e){
+            $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
+            $result = [];
+        }
+        return $result;
+    }
+
+    public function buscar_cita_paciente_mt($mt){
+        try{
+            $sql = "select * from cita c inner join paciente p on c.id_paciente = p.id_paciente where c.cita_mt = ?";
+            $stm = $this->pdo->prepare($sql);
+            $stm->execute([$mt]);
+            $result = $stm->fetch();
         } catch (Exception $e){
             $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
             $result = [];
