0a4544ae26762fe23ae1b5f889754b8462ab8092
Eggcode39 <cesar.ruiz39124@gmail.com>
2021-06-06 01:03:22 -0500

    Productos, marcas y otros

--- app/controllers/ProductoController.php
+++ app/controllers/ProductoController.php
@@ -17,40 +17,28 @@ class ProductoController
         $this->producto = new Producto();
     }
 
-    public function registrar_producto()
+    public function guardar_producto()
     {
         try {
             $model = new Producto();
             //If All OK, the message does not change
             $message = "Code 1: Ok, Code 2: Error al crear Usuario, Code 3: Paciente con DNI ya existente, Code 4: DNI no tiene 8 caracteres";
+            if(isset($_POST['id_producto'])){
+                $model->id_producto = $_POST['id_producto'];
+            }
+            $model->id_marca = $_POST['id_marca'];
+            $model->id_modelo = $_POST['id_modelo'];
             $model->producto_nombre = $_POST['producto_nombre'];
             $model->producto_precio = $_POST['producto_precio'];
-            $model->producto_descripcion = $_POST['producto_descripcion'];
-            $model->producto_modelo = $_POST['producto_modelo'];
-            $model->producto_marca = $_POST['producto_marca'];
+            if(empty($_POST['producto_descripcion'])){
+                $model->producto_descripcion = "";
+            } else {
+                $model->producto_descripcion = $_POST['producto_descripcion'];
+            }
             $model->producto_talla = $_POST['producto_talla'];
+            $model->producto_foto = $_POST['producto_foto'];
             $model->producto_estado = $_POST['producto_estado'];
-            $result = $this->producto->registrar_producto($model);
-        } catch (Exception $e) {
-            $this->log->insert($e->getMessage(), get_class($this) . '|' . __FUNCTION__);
-            $result = 2;
-            $message = "Code 2: General Error";
-        }
-        $response = array("code" => $result, "message" => $message);
-        $data = array("result" => $response);
-        echo json_encode($data);
-    }
-
-    public function registrar_foto()
-    {
-        try {
-            $model = new Producto();
-            //If All OK, the message does not change
-            $message = "Code 1: Ok, Code 2: Error al crear Usuario, Code 3: Paciente con DNI ya existente, Code 4: DNI no tiene 8 caracteres";
-            $model->id_producto = $_POST['id_producto'];
-            $model->foto_url = $_POST['foto_url'];
-            $model->foto_estado = $_POST['foto_estado'];
-            $result = $this->producto->registrar_foto($model);
+            $result = $this->producto->guardar_producto($model);
         } catch (Exception $e) {
             $this->log->insert($e->getMessage(), get_class($this) . '|' . __FUNCTION__);
             $result = 2;
@@ -67,20 +55,10 @@ class ProductoController
         try {
             //If All OK, the message does not change
             $message = "We did it. Your awesome... and beatiful";
-            $productos = $this->producto->listar_productos();
-            foreach ($productos as $p){
-                $fotos = $this->producto->listar_fotos_producto($p->id_producto);
-                $productos_[] = array(
-                    "id_producto" => $p->id_producto,
-                    "producto_nombre" => $p->producto_nombre ?? "",
-                    "producto_precio" => $p->producto_precio ?? "",
-                    "producto_descripcion" => $p->producto_descripcion ?? "",
-                    "producto_modelo" => $p->producto_modelo ?? "",
-                    "producto_marca" => $p->producto_marca ?? "",
-                    "producto_talla" => $p->producto_talla ?? "",
-                    "producto_estado" => $p->producto_estado ?? "",
-                    "producto_fotos" => $fotos
-                );
+            if(isset($_POST['producto_estado'])){
+                $productos = $this->producto->listar_productos($_POST['producto_estado']);
+            } else {
+                $productos = $this->producto->listar_productos_todo();
             }
             $result = 1;
         } catch (Exception $e) {
@@ -90,7 +68,139 @@ class ProductoController
             $message = "Code 2: General Error";
         }
         $response = array("code" => $result, "message" => $message);
-        $data = array("result" => $response, "data" => $productos_);
+        $data = array("result" => $response, "data" => $productos);
+        echo json_encode($data);
+    }
+
+    public function guardar_galeria()
+    {
+        try {
+            $model = new Producto();
+            //If All OK, the message does not change
+            $message = "Code 1: Ok, Code 2: Error al crear Usuario, Code 3: Paciente con DNI ya existente, Code 4: DNI no tiene 8 caracteres";
+            if(isset($_POST['id_galeria'])){
+                $model->id_galeria = $_POST['id_galeria'];
+                $result = $this->producto->guardar_galeria($model);
+            } else {
+                $model->id_producto = $_POST['id_producto'];
+                $model->galeria_foto = $_POST['galeria_foto'];
+                $result = $this->producto->guardar_galeria($model);
+            }
+        } catch (Exception $e) {
+            $this->log->insert($e->getMessage(), get_class($this) . '|' . __FUNCTION__);
+            $result = 2;
+            $message = "Code 2: General Error";
+        }
+        $response = array("code" => $result, "message" => $message);
+        $data = array("result" => $response);
+        echo json_encode($data);
+    }
+
+    public function listar_galeria()
+    {
+        $productos_ = [];
+        try {
+            //If All OK, the message does not change
+            $message = "We did it. Your awesome... and beatiful";
+            $productos = $this->producto->listar_galeria($_POST['id_producto']);
+            $result = 1;
+        } catch (Exception $e) {
+            $this->log->insert($e->getMessage(), get_class($this) . '|' . __FUNCTION__);
+            $datos = [];
+            $result = 2;
+            $message = "Code 2: General Error";
+        }
+        $response = array("code" => $result, "message" => $message);
+        $data = array("result" => $response, "data" => $productos);
+        echo json_encode($data);
+    }
+
+    public function guardar_marca()
+    {
+        try {
+            $model = new Producto();
+            //If All OK, the message does not change
+            $message = "Code 1: Ok, Code 2: Error al crear Usuario, Code 3: Paciente con DNI ya existente, Code 4: DNI no tiene 8 caracteres";
+            if(isset($_POST['id_marca'])){
+                $model->id_marca = $_POST['id_marca'];
+            }
+            $model->marca_nombre = $_POST['marca_nombre'];
+            $model->marca_estado = $_POST['marca_estado'];
+            $result = $this->producto->guardar_marca($model);
+        } catch (Exception $e) {
+            $this->log->insert($e->getMessage(), get_class($this) . '|' . __FUNCTION__);
+            $result = 2;
+            $message = "Code 2: General Error";
+        }
+        $response = array("code" => $result, "message" => $message);
+        $data = array("result" => $response);
+        echo json_encode($data);
+    }
+
+    public function listar_marcas()
+    {
+        $productos_ = [];
+        try {
+            //If All OK, the message does not change
+            $message = "We did it. Your awesome... and beatiful";
+            if(isset($_POST['marca_estado'])){
+                $productos = $this->producto->listar_marca($_POST['marca_estado']);
+            } else {
+                $productos = $this->producto->listar_marca_todo();
+            }
+            $result = 1;
+        } catch (Exception $e) {
+            $this->log->insert($e->getMessage(), get_class($this) . '|' . __FUNCTION__);
+            $datos = [];
+            $result = 2;
+            $message = "Code 2: General Error";
+        }
+        $response = array("code" => $result, "message" => $message);
+        $data = array("result" => $response, "data" => $productos);
+        echo json_encode($data);
+    }
+
+    public function guardar_modelo()
+    {
+        try {
+            $model = new Producto();
+            //If All OK, the message does not change
+            $message = "Code 1: Ok, Code 2: Error al crear Usuario, Code 3: Paciente con DNI ya existente, Code 4: DNI no tiene 8 caracteres";
+            if(isset($_POST['id_modelo'])){
+                $model->id_modelo = $_POST['id_modelo'];
+            }
+            $model->modelo_nombre = $_POST['modelo_nombre'];
+            $model->modelo_estado = $_POST['modelo_estado'];
+            $result = $this->producto->guardar_modelo($model);
+        } catch (Exception $e) {
+            $this->log->insert($e->getMessage(), get_class($this) . '|' . __FUNCTION__);
+            $result = 2;
+            $message = "Code 2: General Error";
+        }
+        $response = array("code" => $result, "message" => $message);
+        $data = array("result" => $response);
+        echo json_encode($data);
+    }
+
+    public function listar_modelos()
+    {
+        try {
+            //If All OK, the message does not change
+            $message = "We did it. Your awesome... and beatiful";
+            if(isset($_POST['modelo_estado'])){
+                $productos = $this->producto->listar_modelo($_POST['modelo_estado']);
+            } else {
+                $productos = $this->producto->listar_modelo_todo();
+            }
+            $result = 1;
+        } catch (Exception $e) {
+            $this->log->insert($e->getMessage(), get_class($this) . '|' . __FUNCTION__);
+            $datos = [];
+            $result = 2;
+            $message = "Code 2: General Error";
+        }
+        $response = array("code" => $result, "message" => $message);
+        $data = array("result" => $response, "data" => $productos);
         echo json_encode($data);
     }
 }
--- app/models/Producto.php
+++ app/models/Producto.php
@@ -13,19 +13,36 @@ class Producto{
         $this->pdo = Database::getConnection();
     }
 
-    public function registrar_producto($model){
+    public function guardar_producto($model){
         try{
-            $sql = "insert into producto (producto_nombre, producto_precio, producto_descripcion, producto_modelo, producto_marca, producto_talla, producto_estado) values (?,?,?,?,?,?,?)";
-            $stm = $this->pdo->prepare($sql);
-            $stm->execute([
-                $model->producto_nombre,
-                $model->producto_precio,
-                $model->producto_descripcion,
-                $model->producto_modelo,
-                $model->producto_marca,
-                $model->producto_talla,
-                $model->producto_estado
-            ]);
+            if(isset($model->id_producto)){
+                $sql = "update producto set id_marca = ?, id_modelo = ?, producto_nombre = ?, producto_precio = ?, producto_descripcion = ?, producto_talla = ?, producto_foto = ?, producto_estado = ? where id_producto = ?";
+                $stm = $this->pdo->prepare($sql);
+                $stm->execute([
+                    $model->id_marca,
+                    $model->id_modelo,
+                    $model->producto_nombre,
+                    $model->producto_precio,
+                    $model->producto_descripcion,
+                    $model->producto_talla,
+                    $model->producto_foto,
+                    $model->producto_estado,
+                    $model->id_producto
+                ]);
+            } else {
+                $sql = "insert into producto (id_marca, id_modelo, producto_nombre, producto_precio, producto_descripcion, producto_talla, producto_foto, producto_estado) values (?,?,?,?,?,?,?,?)";
+                $stm = $this->pdo->prepare($sql);
+                $stm->execute([
+                    $model->id_marca,
+                    $model->id_modelo,
+                    $model->producto_nombre,
+                    $model->producto_precio,
+                    $model->producto_descripcion,
+                    $model->producto_talla,
+                    $model->producto_foto,
+                    $model->producto_estado
+                ]);
+            }
             $result = 1;
         } catch (Exception $e){
             $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
@@ -34,26 +51,22 @@ class Producto{
         return $result;
     }
 
-    public function registrar_foto($model){
+    public function listar_productos($producto_estado){
         try{
-            $sql = "insert into foto (id_producto, foto_url, foto_estado) values (?,?,?)";
+            $sql = "select * from producto p inner join marca m on p.id_marca = m.id_marca inner join modelo m2 on p.id_modelo = m2.id_modelo where p.producto_estado = ?";
             $stm = $this->pdo->prepare($sql);
-            $stm->execute([
-                $model->id_producto,
-                $model->foto_url,
-                $model->foto_estado
-            ]);
-            $result = 1;
+            $stm->execute([$producto_estado]);
+            $result = $stm->fetchAll();
         } catch (Exception $e){
             $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
-            $result = 2;
+            $result = [];
         }
         return $result;
     }
 
-    public function listar_productos(){
+    public function listar_productos_todo(){
         try{
-            $sql = "select * from producto";
+            $sql = "select * from producto p inner join marca m on p.id_marca = m.id_marca inner join modelo m2 on p.id_modelo = m2.id_modelo";
             $stm = $this->pdo->prepare($sql);
             $stm->execute();
             $result = $stm->fetchAll();
@@ -64,11 +77,139 @@ class Producto{
         return $result;
     }
 
-    public function listar_fotos_producto($id_producto){
+    public function guardar_galeria($model){
         try{
-            $sql = "select * from foto where id_producto = ?";
+            if(isset($model->id_galeria)){
+                $sql = "delete from galeria where id_galeria = ?";
+                $stm = $this->pdo->prepare($sql);
+                $stm->execute([
+                    $model->id_galeria
+                ]);
+            } else {
+                $sql = "insert into galeria (id_producto, galeria_foto) values (?,?)";
+                $stm = $this->pdo->prepare($sql);
+                $stm->execute([
+                    $model->id_producto,
+                    $model->galeria_foto
+                ]);
+            }
+            $result = 1;
+        } catch (Exception $e){
+            $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
+            $result = 2;
+        }
+        return $result;
+    }
+
+    public function listar_galeria($id_producto){
+        try{
+            $sql = "select * from galeria where id_producto = ?";
             $stm = $this->pdo->prepare($sql);
             $stm->execute([$id_producto]);
+            $result = $stm->fetchAll();
+        } catch (Exception $e){
+            $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
+            $result = [];
+        }
+        return $result;
+    }
+
+    public function guardar_marca($model){
+        try{
+            if(isset($model->id_marca)){
+                $sql = "update marca set marca_nombre = ?, marca_estado = ? where id_marca = ?";
+                $stm = $this->pdo->prepare($sql);
+                $stm->execute([
+                    $model->marca_nombre,
+                    $model->marca_estado,
+                    $model->id_marca
+                ]);
+            } else {
+                $sql = "insert into marca (marca_nombre, marca_estado) values (?,?)";
+                $stm = $this->pdo->prepare($sql);
+                $stm->execute([
+                    $model->marca_nombre,
+                    $model->marca_estado
+                ]);
+            }
+            $result = 1;
+        } catch (Exception $e){
+            $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
+            $result = 2;
+        }
+        return $result;
+    }
+
+    public function listar_marca($marca_estado){
+        try{
+            $sql = "select * from marca where marca_estado = ?";
+            $stm = $this->pdo->prepare($sql);
+            $stm->execute([$marca_estado]);
+            $result = $stm->fetchAll();
+        } catch (Exception $e){
+            $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
+            $result = [];
+        }
+        return $result;
+    }
+
+    public function listar_marca_todo(){
+        try{
+            $sql = "select * from marca";
+            $stm = $this->pdo->prepare($sql);
+            $stm->execute();
+            $result = $stm->fetchAll();
+        } catch (Exception $e){
+            $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
+            $result = [];
+        }
+        return $result;
+    }
+
+    public function guardar_modelo($model){
+        try{
+            if(isset($model->id_modelo)){
+                $sql = "update modelo set modelo_nombre = ?, modelo_estado = ? where id_modelo = ?";
+                $stm = $this->pdo->prepare($sql);
+                $stm->execute([
+                    $model->modelo_nombre,
+                    $model->modelo_estado,
+                    $model->id_modelo
+                ]);
+            } else {
+                $sql = "insert into modelo (modelo_nombre, modelo_estado) values (?,?)";
+                $stm = $this->pdo->prepare($sql);
+                $stm->execute([
+                    $model->modelo_nombre,
+                    $model->modelo_estado
+                ]);
+            }
+            $result = 1;
+        } catch (Exception $e){
+            $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
+            $result = 2;
+        }
+        return $result;
+    }
+
+    public function listar_modelo($modelo_estado){
+        try{
+            $sql = "select * from modelo where modelo_estado = ?";
+            $stm = $this->pdo->prepare($sql);
+            $stm->execute([$modelo_estado]);
+            $result = $stm->fetchAll();
+        } catch (Exception $e){
+            $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
+            $result = [];
+        }
+        return $result;
+    }
+
+    public function listar_modelo_todo(){
+        try{
+            $sql = "select * from modelo";
+            $stm = $this->pdo->prepare($sql);
+            $stm->execute();
             $result = $stm->fetchAll();
         } catch (Exception $e){
             $this->log->insert($e->getMessage(), get_class($this).'|'.__FUNCTION__);
